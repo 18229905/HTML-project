8f3b54832980d445336213dbb5c1878134c04439
18229905 <18229905@life.hkbu.edu.hk>
2020-10-22 11:45:07 +0800

    Modify the button color of the create form

--- config/routes.js
+++ config/routes.js
@@ -24,6 +24,7 @@ module.exports.routes = {
  
 
   '/': { view: 'qpon/homepage' },
+  'GET /': 'QponController.show',
 
 
   /***************************************************************************
